31bcaad576baed847ecb6c34740b62ea15c16ca9
Eigensystem <eigensystem1318@gmail.com>
2022-05-05 16:02:46 +0800

    fix

--- os/target/riscv64gc-unknown-none-elf/debug/deps/os-d34d83c6eb2cad06.d
+++
@@ -1,9 +0,0 @@
-/home/parallels/ComputerScience/System/RISC-V/os/target/riscv64gc-unknown-none-elf/debug/deps/os-d34d83c6eb2cad06.rmeta: src/main.rs src/sbi.rs src/console.rs src/lang_items.rs src/entry.asm
-
-/home/parallels/ComputerScience/System/RISC-V/os/target/riscv64gc-unknown-none-elf/debug/deps/os-d34d83c6eb2cad06.d: src/main.rs src/sbi.rs src/console.rs src/lang_items.rs src/entry.asm
-
-src/main.rs:
-src/sbi.rs:
-src/console.rs:
-src/lang_items.rs:
-src/entry.asm:
--- os/target/riscv64gc-unknown-none-elf/debug/deps/os-fdf89fa0e7a60c24.d
+++
@@ -1,9 +0,0 @@
-/home/parallels/ComputerScience/System/RISC-V/os/target/riscv64gc-unknown-none-elf/debug/deps/os-fdf89fa0e7a60c24.rmeta: src/main.rs src/sbi.rs src/console.rs src/lang_items.rs src/entry.asm
-
-/home/parallels/ComputerScience/System/RISC-V/os/target/riscv64gc-unknown-none-elf/debug/deps/os-fdf89fa0e7a60c24.d: src/main.rs src/sbi.rs src/console.rs src/lang_items.rs src/entry.asm
-
-src/main.rs:
-src/sbi.rs:
-src/console.rs:
-src/lang_items.rs:
-src/entry.asm:
